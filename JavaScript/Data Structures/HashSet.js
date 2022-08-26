f3e2ea2a12b316323a5440e135f2e9f44ed71cfe
TempUsername2436 <c.benton.145@gmail.com>
2022-08-26 09:37:42 -0700

    Added hashset

---
+++ JavaScript/Data Structures/HashSet.js
@@ -0,0 +1,40 @@
+class HashSet {
+  constructor(elements = []) {
+    if (!Array.isArray(elements)) throw new Error(`Iterator value ${elements} is not an entry object`);
+    this.elements = {};
+    this.size = 0;
+
+    for (let i = 0; i < elements.length; i++) {
+      if (!this.has(elements[i])) this.size++;
+      this.elements[elements[i]] = elements[i];
+    }
+  }
+
+  add(value) {
+    if (!this.has(value)) this.size++;
+    this.elements[value] = value;
+  }
+
+  has(value) {
+    return (this.elements[value]) ? true : false;
+  }
+
+  delete(value) {
+    if (!this.has(value)) return false;
+    delete this.elements[value];
+    this.size--;
+
+    return true;
+  }
+
+  forEach(_function) {
+    for (const value in this.elements) _function(this.elements[value]);
+  }
+
+  clear() {
+    this.elements = {};
+    this.size = 0;
+  }
+}
+
+export default HashSet;
